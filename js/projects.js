17d029484ce4e4c6a2a9198339bb0d974edb8553
Liz <51245710+eahowell@users.noreply.github.com>
2025-01-14 22:14:43 -0600

    Adjusting Nav Bar Settings

--- js/projects.js
+++ js/projects.js
@@ -1,22 +1,30 @@
 // Define showProjectDetails globally
-window.showProjectDetails = function(projectId) {
-    fetch("projects.json")
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return response.json();
-        })
-        .then(data => {
-            const project = data.projects.find(p => p.id === projectId);
-            if (project) {
-                document.getElementById("DetailsModalCenterTitle").textContent = project.title;
+window.showProjectDetails = function (projectId) {
+  fetch("projects.json")
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      const project = data.projects.find((p) => p.id === projectId);
+      if (project) {
+        document.getElementById("DetailsModalCenterTitle").textContent =
+          project.title;
 
-                const modalBody = document.getElementById("modalBody");
-modalBody.innerHTML = `
-    <img src="${project.logoImage.src}" alt="${project.logoImage.alt}" style="height: ${project.logoImage.style.height};">
+        const modalBody = document.getElementById("modalBody");
+        modalBody.innerHTML = `
+    <img src="${project.logoImage.src}" alt="${
+          project.logoImage.alt
+        }" style="height: ${project.logoImage.style.height};">
     <br>
-    ${project.links.map(link => `<a class="project-item-links button1" href="${link.url}" target="_blank" title="${link.title}">${link.text}</a>`).join('')}
+    ${project.links
+      .map(
+        (link) =>
+          `<a class="project-item-links button1" href="${link.url}" target="_blank" title="${link.title}">${link.text}</a>`
+      )
+      .join("")}
     <br><br>
     <h2>Description</h2>
     <p class="text-start">${project.description}</p>
@@ -26,58 +34,70 @@ modalBody.innerHTML = `
     <br/>
     <h3>Project Images</h3>
     <div class="project-images">
-        ${project.images.map(image => `
+        ${project.images
+          .map(
+            (image) => `
             <div class="project-image-container">
                 <img src="${image.src}" alt="${image.alt}" style="height: ${image.style.height};">
             </div>
-        `).join('')}
+        `
+          )
+          .join("")}
     </div>
     <br/>
 
     <h3>My role was to build:</h3>
     <ul class="list-group">
-        ${project.role.map(item => `<li class="list-group-item text-start">${item}</li>`).join('')}
+        ${project.role
+          .map((item) => `<li class="list-group-item text-start">${item}</li>`)
+          .join("")}
     </ul>
     <br/>
     <h3>I learned to:</h3>
     <ul class="list-group">
-        ${project.learnings.map(item => `<li class="list-group-item text-start">${item}</li>`).join('')}
+        ${project.learnings
+          .map((item) => `<li class="list-group-item text-start">${item}</li>`)
+          .join("")}
     </ul>
     <br/>
     <h3>Technologies Used</h3>
     <ul class="list-group list-group-horizontal flex-wrap">
-        ${project.technologies.map(tech => `<li class="list-group-item text-start">${tech}</li>`).join('')}
+        ${project.technologies
+          .map((tech) => `<li class="list-group-item text-start">${tech}</li>`)
+          .join("")}
     </ul>
     
 `;
 
-                const modal = new bootstrap.Modal(document.getElementById("ProjectDetailsModalCenter"));
-                modal.show();
-            }
-        })
-        .catch(error => {
-            console.error('There was a problem with the fetch operation:', error);
-        });
+        const modal = new bootstrap.Modal(
+          document.getElementById("ProjectDetailsModalCenter")
+        );
+        modal.show();
+      }
+    })
+    .catch((error) => {
+      console.error("There was a problem with the fetch operation:", error);
+    });
 };
 
 // Load and process the JSON data
-document.addEventListener('DOMContentLoaded', function() {
-    fetch("projects.json")
-        .then(response => {
-            if (!response.ok) {
-                throw new Error('Network response was not ok');
-            }
-            return response.json();
-        })
-        .then(data => {
-            const projectList = document.getElementById("projectList");
+document.addEventListener("DOMContentLoaded", function () {
+  fetch("projects.json")
+    .then((response) => {
+      if (!response.ok) {
+        throw new Error("Network response was not ok");
+      }
+      return response.json();
+    })
+    .then((data) => {
+      const projectList = document.getElementById("projectList");
 
-            data.projects.forEach(project => {
-                const projectItem = document.createElement("div");
-                projectItem.className = "project-list__item";
-                projectItem.innerHTML = `
+      data.projects.forEach((project) => {
+        const projectItem = document.createElement("div");
+        projectItem.className = "project-list__item";
+        projectItem.innerHTML = `
                     <div class="project-header">
-                        <h3>${project.title}</h3>
+                        <h3 class="project-header-title">${project.title}</h3>
                         <img src="${project.logoImage.src}" alt="${project.logoImage.alt}" style="height: ${project.logoImage.style.height};">
                     </div>
                     <div class="project-content">
@@ -88,10 +108,10 @@ document.addEventListener('DOMContentLoaded', function() {
                         </div>
                     </div>
                 `;
-                projectList.appendChild(projectItem);
-            });
-        })
-        .catch(error => {
-            console.error('There was a problem with the fetch operation:', error);
-        });
+        projectList.appendChild(projectItem);
+      });
+    })
+    .catch((error) => {
+      console.error("There was a problem with the fetch operation:", error);
+    });
 });
